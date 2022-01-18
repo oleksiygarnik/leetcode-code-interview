df7e77d456534d64499d95f9bce29b67778692d6
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-01-18 18:55:58 +0200

    Time: 181 ms (21.85%), Space: 39.8 MB (79.63%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool CanPlaceFlowers(int[] flowerbed, int n) {
+         int i = 0, count = 0;
+        while (i < flowerbed.Length) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++;
+            }
+             if(count>=n)
+                return true;
+            i++;
+        }
+        return false;
+    }
+}
