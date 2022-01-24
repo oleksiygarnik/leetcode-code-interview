4f2e62d65196793c64cc020aedf6005bbd9ad3fe
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-01-24 22:18:03 +0200

    Time: 83 ms (51.58%), Space: 36.4 MB (93.68%) - LeetHub

--- 520-detect-capital/520-detect-capital.cs
+++ 520-detect-capital/520-detect-capital.cs
@@ -1,44 +1,9 @@
 public class Solution {
     public bool DetectCapitalUse(string word)
     {
-        bool isRight = false;
-        var firstLetterIsUpper = char.IsUpper(word[0]);
-        var isLowerExist = false;
-        var isUpperOnly = false;
-        for(int i = 1; i < word.Length; i++)
-        {
-            var isUpper = char.IsUpper(word[i]);
-            if(isUpper)
-                isUpperOnly = true;
-            
-            if(!firstLetterIsUpper)
-           {
-               if(isUpper)
-                   return false;
-           }
-            else{
-                if(isLowerExist)
-                {
-                    if(isUpper)
-                        return false;
-                    
-                    isLowerExist = true;
-                    continue;
-                }
-                else{
-                    if(!isUpper)
-                    {
-                        if(isUpperOnly)
-                            return false;
-                        
-                        isLowerExist = true;
-                    }
-                }
-                
-                
-            }
-        }
-        
-        return true;
+        var count = word.Count(x => char.IsUpper(x));
+        return count == word.Length 
+            || count == 0 
+            || (count == 1 && char.IsUpper(word[0]));
     }
 }
