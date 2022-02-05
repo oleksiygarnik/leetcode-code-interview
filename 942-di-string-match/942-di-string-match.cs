4dbcb409feaea315769cf646dfb567718f6951ab
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-02-05 20:42:38 +0200

    Time: 172 ms (58.57%), Space: 46.1 MB (7.14%) - LeetHub

--- 942-di-string-match/942-di-string-match.cs
+++ 942-di-string-match/942-di-string-match.cs
@@ -1,25 +1,23 @@
 public class Solution {
     public int[] DiStringMatch(string s) {
         var result = new List<int>();
-        var numbers = Enumerable.Range(0, s.Length + 1).ToArray();
         var left = 0;
-        var right = numbers.Length - 1;
+        var right = s.Length;
         for(int i = 0; i < s.Length; i++)
         {
             if(s[i] == 'I')
             {
-                result.Add(numbers[left]);
+                result.Add(left);
                 left++;
             }
-            
-            if(s[i] == 'D')
+            else
             {
-                result.Add(numbers[right]);
+                result.Add(right);
                 right--;
             }
         }
         
-        result.Add(numbers[left]);
+        result.Add(left);
         
         return result.ToArray();
     }
