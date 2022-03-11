0c27df470098cd31c78916e3ca80d1f51ad21243
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-11 10:12:28 +0200

    Time: 167 ms (75.17%), Space: 46.5 MB (39.24%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cs
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cs
@@ -2,23 +2,9 @@ public class Solution {
     public IList<IList<int>> AllPathsSourceTarget(int[][] graph) 
     {
         var result = new List<IList<int>>();
-        
-        var path = new List<int>();
-        path.Add(0);
         var visited = new bool[graph.Length];
         
-        foreach(var neighbour in graph[0])
-        {
-            if(!visited[neighbour])
-            {
-                path.Add(neighbour);
-                Dfs(result, graph, path, visited, neighbour);
-                
-                visited[neighbour] = false;
-                path.RemoveAt(path.Count - 1);
-                
-            }
-        }
+        Dfs(result, graph, new List<int>(){ 0 }, visited, 0);
         
         return result;
     }
@@ -40,124 +26,10 @@ public class Solution {
             {
                 path.Add(neighbour);
                 Dfs(result, graph, path, visited, neighbour);
-                
+        
                 visited[neighbour] = false;
                 path.RemoveAt(path.Count - 1);
             }
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// public class Solution {
-    
-//     public static IList<IList<int>> result;
-//     public IList<IList<int>> AllPathsSourceTarget(int[][] graph) {
-//         result = new List<IList<int>>();
-        
-//         var maxElem = graph.SelectMany(x => x).Max();
-//         var visited = new bool[maxElem + 1];
-//         var stack = new Stack<int>();
-//         stack.Push(0);
-        
-//         var path = new List<int>();
-//         while(stack.Count() > 0)
-//         {
-//             var currentVertex = stack.Pop();
-            
-//             if(visited[currentVertex] == true)
-//                 continue;
-            
-//             visited[currentVertex] = true;
-//             path.Add(currentVertex);
-            
-//             if(currentVertex == maxElem)
-//             {
-//                 var pathResult = path.GetRange(0, path.Count);
-//                 result.Add(pathResult);
-//                 break;
-//             }
-            
-//             foreach(var nextVertex in graph[currentVertex])
-//             {
-//                 if(visited[nextVertex] == false)
-//                 {
-//                     stack.Push(nextVertex);
-//                     SearchUtil(visited, path, stack, graph, maxElem);
-                    
-//                     path.RemoveAt(path.Count - 1);
-//                     visited[nextVertex] = false;
-//                 }
-//             }
-//         }
-        
-//         return result;
-//     }
-    
-//     private void SearchUtil(bool[] visited, List<int> path, Stack<int> stack, int[][] graph, int maxElem)
-//     {
-//             var currentVertex = stack.Pop();
-            
-//             if(visited[currentVertex] == true)
-//                 return;
-            
-//             visited[currentVertex] = true;
-//             path.Add(currentVertex);
-            
-//             if(currentVertex == maxElem)
-//             {
-//                 var pathResult = path.GetRange(0, path.Count);
-//                 result.Add(pathResult);
-//                 return;
-//             }
-            
-//             foreach(var nextVertex in graph[currentVertex])
-//             {
-//                 if(visited[nextVertex] == false)
-//                 {
-//                     stack.Push(nextVertex);
-//                     SearchUtil(visited, path, stack, graph, maxElem);
-                    
-//                     //Reset
-//                     path.RemoveAt(path.Count - 1);
-//                     visited[nextVertex] = false;
-//                 }
-//             }
-//     }
-// }
