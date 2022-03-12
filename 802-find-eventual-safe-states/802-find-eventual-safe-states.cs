adbf23003e09509635759b39c36bf650fb52ea2e
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-12 13:09:51 +0200

    Time: 326 ms (88.10%), Space: 50 MB (97.62%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cs
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cs
@@ -2,18 +2,17 @@ public class Solution
 {
     public List<int> EventualSafeNodes(int[][] graph) 
     {
-        List<int> res = new List<int>();
-        int[] color = new int[graph.Length];
+        var result = new List<int>();
+        var color = new int[graph.Length];
 
-        for(int i =+ 0; i < graph.Length; i++)
-        {
+        for(int i = 0; i < graph.Length; i++)
             if(Dfs(graph, i, color))
-                res.Add(i);
-        }
+                result.Add(i);
 
-        return res;
+        return result;
     }
     
+    // 2 = safe node, 1 = 
     private bool Dfs(int[][] graph, int node, int[] color)
     {
         if(color[node] == 2)
@@ -32,16 +31,3 @@ public class Solution
         return true;
     }
 }
-
-
-// private int dfs(int[][] adj, int src, int[] vis){
-//     if(vis[src] != 0) return vis[src];//0 represents unvisited node
-
-//     vis[src] = -1;//-1 represents current search
-
-//     foreach(int neighbor in adj[src])
-//         if(dfs(adj, neighbor, vis) == -1) 
-//             return -1;//unsafe node return immediately 
-
-//     return vis[src] = 1;//1 represents safe node
-// }
