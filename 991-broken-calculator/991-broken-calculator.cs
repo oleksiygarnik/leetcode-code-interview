614b85b48516042a057343dc9f10258471fc5c7a
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-23 09:26:29 +0200

    Time: 1677 ms (6.25%), Space: 25.8 MB (6.25%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cs
@@ -0,0 +1,61 @@
+public class Solution {
+    public int BrokenCalc(int startValue, int target) 
+    {
+        int ans = 0;
+        while (target != startValue) {
+            ans++;
+            if (target < startValue || target % 2 == 1)
+                target++;
+            else
+                target /= 2;
+        }
+
+        return ans; //+ startValue - target;
+    }
+    
+//     private int GetNum(int currentValue, int target, int distance)
+//     {
+//         if(currentValue == target)
+//             return distance;
+        
+//         Console.WriteLine(currentValue);
+        
+//         if(currentValue > target)
+//             return GetNum(currentValue - 1, target, ++distance);
+        
+//         return GetNum(currentValue * 2, target, ++distance);
+//     }
+    
+    
+    
+    //         var unique = new HashSet<int>();
+//         unique.Add(startValue);
+        
+//         var queue = new Queue<int>(startValue);
+//         queue.Enqueue(startValue);
+        
+//         int distance = 0;
+//         while(queue.Count() > 0)
+//         {
+//             var size = queue.Count();
+//             while(size-- > 0)
+//             {
+//                 var current = queue.Dequeue();
+                
+//                 if(current == target)
+//                     return distance;
+                
+//                 var num = current * 2;
+//                 if(current < target && !unique.Contains(num))
+//                     queue.Enqueue(num);
+                
+//                 num = current - 1;
+//                 if(!unique.Contains(num))
+//                     queue.Enqueue(num);
+//             }
+            
+//             distance++;
+//         }
+        
+//         return distance;
+}
