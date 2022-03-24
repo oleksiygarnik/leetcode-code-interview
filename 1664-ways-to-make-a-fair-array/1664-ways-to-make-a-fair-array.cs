ed4aa9a85ba1b73a8f1dc9e229a77552849c02ac
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-24 17:57:52 +0200

    Time: 486 ms (6.25%), Space: 46.7 MB (31.25%) - LeetHub

--- 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cs
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.cs
@@ -3,7 +3,6 @@ public class Solution {
     {
         if(nums.Length == 1)
             return 1;
-        
         if(nums.Length == 2)
             return 0;
         
@@ -14,32 +13,29 @@ public class Solution {
         for(int i = 2; i < nums.Length; i++)
             prefixSum[i] = prefixSum[i - 2] + nums[i];
         
-        var n = prefixSum.Length;
         int odd = 0;
         int even = 0;
         var result = 0;
         for(int i = nums.Length - 1; i >= 1; i--)
         {
+            int first, second = 0; 
             if(i % 2 == 0)
             {
-                var first = prefixSum[i] - nums[i] + odd;
-                var second = prefixSum[i - 1] + even; 
-              
-                if(first == second)
-                    result++;
+                first = prefixSum[i] - nums[i] + odd;
+                second = prefixSum[i - 1] + even; 
                 
                 even += nums[i];
             }
             else
             {
-                var first = prefixSum[i - 1] + odd;
-                var second = prefixSum[i] - nums[i] + even;
-                
-                if(first == second)
-                    result++;
+                first = prefixSum[i - 1] + odd;
+                second = prefixSum[i] - nums[i] + even;
                 
                 odd += nums[i];
             }
+            
+             if(first == second)
+                 result++;
         }
 
         if(odd == even)
