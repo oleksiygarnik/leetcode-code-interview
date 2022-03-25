3f3d94d70c7cc5e1ee278e3c293ff9cc35cfefd4
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-25 11:00:29 +0200

    Time: 125 ms (43.75%), Space: 37.7 MB (45.31%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cs
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cs
@@ -1,30 +1,33 @@
 public class Solution 
 {
-    public int TwoCitySchedCost(int[][] costs) 
-    {
-//         var priority = new (int, int)[costs.Length];
-//         for(int i = 0; i < costs.Length; i++)
-//             priority[i] = (costs[i][0] - costs[i][1], i);
-        
-//         Array.Sort(priority, (x, y) => x.Item1.CompareTo(y.Item1));
+//     public int TwoCitySchedCost(int[][] costs) 
+//     {
+//         Array.Sort(costs, (x, y) => (x[0] - x[1]).CompareTo(y[0] - y[1]));
         
 //         var result = 0;
 //         for(int i = 0; i < costs.Length/2; i++)
-//             result+=costs[priority[i].Item2][0] + costs[priority[i + costs.Length/2].Item2][1];
+//             result += costs[i][0] + costs[i + costs.Length/2][1]; 
         
 //         return result;
-        
-        Array.Sort(costs, new Comparer());
-        
-        var result = 0;
-        for(int i = 0; i < costs.Length/2; i++)
-            result += costs[i][0] + costs[i + costs.Length/2][1]; 
-        
-        return result;
-    }
+//     }
     
-    public class Comparer : IComparer<int[]>
-    {
-        public int Compare(int[] x, int[] y) => (x[0] - x[1]).CompareTo(y[0] - y[1]);
-    }
+        public int TwoCitySchedCost(int[][] costs) 
+        {
+            int N = costs.Length / 2;
+            int[][] dp = new int[N + 1][];
+            for(int i = 0; i < dp.Length; i++)
+                dp[i] = new int[N +1];
+            
+            for (int i = 1; i <= N; i++) 
+                dp[i][0] = dp[i - 1][0] + costs[i - 1][0];
+
+            for (int j = 1; j <= N; j++)
+                dp[0][j] = dp[0][j - 1] + costs[j - 1][1];
+
+            for (int i = 1; i <= N; i++) 
+                for (int j = 1; j <= N; j++) 
+                    dp[i][j] = Math.Min(dp[i - 1][j] + costs[i + j - 1][0], dp[i][j - 1] + costs[i + j - 1][1]);
+
+            return dp[N][N];
+        }
 }
