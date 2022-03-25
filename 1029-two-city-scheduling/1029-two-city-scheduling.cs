455117becb2514bba4e36ea8e49d1d2eae71a91f
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-25 10:23:35 +0200

    Time: 90 ms (82.81%), Space: 37 MB (75.00%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cs
@@ -0,0 +1,20 @@
+public class Solution 
+{
+    public int TwoCitySchedCost(int[][] costs) 
+    {
+        var priority = new (int, int)[costs.Length];
+        for(int i = 0; i < costs.Length; i++)
+            priority[i] = (costs[i][0] - costs[i][1], i);
+        
+        Array.Sort(priority, (x, y) => x.Item1.CompareTo(y.Item1));
+        
+        var result = 0;
+        for(int i = 0; i < costs.Length/2; i++)
+            result+=costs[priority[i].Item2][0];
+        
+        for(int i = costs.Length/2; i < costs.Length; i++)
+            result+=costs[priority[i].Item2][1];
+
+        return result;
+    }
+}
