9d215fb0bd2da53906df5417d9d0f0648d9af8fb
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-26 14:17:21 +0200

    Time: 159 ms (17.55%), Space: 39.8 MB (24.95%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cs
@@ -0,0 +1,52 @@
+public class Solution 
+{
+    private static int[] rowDirections = new int[]{ 0, -1, 0, 1};
+    private static int[] columnDirections = new int[]{ -1, 0, 1, 0};
+    
+    public int MinPathSum(int[][] grid) 
+    {
+        var queue = new Queue<Tuple<int, int>>();
+        queue.Enqueue(Tuple.Create(0, 0));
+        
+        var visited = new bool[grid.Length, grid[0].Length];
+        visited[0,0] = true;
+        
+        while(queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            var row = current.Item1;
+            var column = current.Item2;
+            
+            if(row == grid.Length - 1 && column == grid[row].Length - 1)
+                return grid[row][column];
+            
+            for(int i = 0; i < 4; i++)
+            {
+                var newRow = row + rowDirections[i];
+                var newColumn = column + columnDirections[i];
+                
+                if(IsValid(grid, newRow, newColumn) && !visited[newRow,newColumn])
+                {
+                    visited[newRow,newColumn] = true;
+                    queue.Enqueue(Tuple.Create(newRow, newColumn));
+                    
+                    if(newRow == 0 || newColumn == 0)
+                        grid[newRow][newColumn] += grid[row][column];
+                    else if(newRow > 0 && newRow < grid.Length && newColumn < grid[newRow].Length && newColumn > 0)
+                        grid[newRow][newColumn] += Math.Min(grid[newRow - 1][newColumn], grid[newRow][newColumn - 1]);
+                    // Console.WriteLine(newRow + " " + newColumn + ": " + grid[newRow][newColumn]);
+                } 
+            }
+        }
+        
+        return -1;
+    }
+    
+    private bool IsValid(int[][] grid, int row, int column)
+    {
+        if(row < 0 || column < 0 || row > grid.Length - 1 || column > grid[row].Length - 1)
+            return false;
+        
+        return true;
+    }
+}
