2ac6f305a5bc4babbd6d2f83235c115d0c8f6872
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-26 15:52:08 +0200

    Time: 200 ms (5.08%), Space: 38.5 MB (39.59%) - LeetHub

--- 120-triangle/120-triangle.cs
+++ 120-triangle/120-triangle.cs
@@ -1,20 +1,39 @@
 public class Solution 
 {
-    public int MinimumTotal(IList<IList<int>> triangle) 
+     public int MinimumTotal(IList<IList<int>> triangle) 
     {
-        for(int i = 1; i < triangle.Count(); i++)
-        {
+        for(int i = triangle.Count() - 2; i >= 0; i--)
             for(int j = 0; j < triangle[i].Count(); j++)
-            {
-                if(j == 0)
-                    triangle[i][j] += triangle[i - 1][j];
-                else if(j == triangle[i].Count() - 1)
-                    triangle[i][j] += triangle[i - 1][j - 1];
-                else 
-                    triangle[i][j] += Math.Min(triangle[i - 1][j - 1], triangle[i - 1][j]);
-            }
-        }
+                triangle[i][j] += Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
         
-        return triangle[triangle.Count() - 1].Min();
+        return triangle[0][0];
     }
+    
+//     public int MinimumTotal(IList<IList<int>> triangle) 
+//     {
+//         var num = new int[triangle.Count() + 1];
+//         for(int i = triangle.Count() - 1; i >= 0; i--)
+//             for(int j = 0; j < triangle[i].Count(); j++)
+//                 num[j] = Math.Min(num[j], num[j + 1]) + triangle[i][j];
+        
+//         return num[0];
+//     }
+    
+//     public int MinimumTotal(IList<IList<int>> triangle) 
+//     {
+//         for(int i = 1; i < triangle.Count(); i++)
+//         {
+//             for(int j = 0; j < triangle[i].Count(); j++)
+//             {
+//                 if(j == 0)
+//                     triangle[i][j] += triangle[i - 1][j];
+//                 else if(j == triangle[i].Count() - 1)
+//                     triangle[i][j] += triangle[i - 1][j - 1];
+//                 else 
+//                     triangle[i][j] += Math.Min(triangle[i - 1][j - 1], triangle[i - 1][j]);
+//             }
+//         }
+        
+//         return triangle[triangle.Count() - 1].Min();
+//     }
 }
