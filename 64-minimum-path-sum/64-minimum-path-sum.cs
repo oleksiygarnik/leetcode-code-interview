15fa2e933518ddab9f7df5ab2cdd068423f3b02b
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-26 15:01:43 +0200

    Time: 114 ms (66.81%), Space: 38.5 MB (86.47%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cs
+++ 64-minimum-path-sum/64-minimum-path-sum.cs
@@ -2,37 +2,21 @@ public class Solution
 {
     public int MinPathSum(int[][] grid) 
     {
-        int m = grid.Length;
-        int n = grid[0].Length;
-        
-        var dp = new int[m, n];
-        for(int i = 0; i < m ; i++)
+        int m = grid.Length, n = grid[0].Length;
+        for(int i = 0; i < m; i++)
         {
-            if(i == 0)
-                dp[i, 0] = grid[0][0];
-            else
-                dp[i, 0] = dp[i - 1, 0] + grid[i][0];
-        }
-        
-         for(int i = 1; i < n ; i++)
-         {
-             if(i == 0)
-                dp[0,i] = grid[0][0];
-            else
-                dp[0, i] = dp[0, i - 1] + grid[0][i];
-         }
-        
-        for(int i = 1; i < m; i++)
-        {
-            for(int j = 1; j < n; j++)
+            for(int j = 0; j < n; j++)
             {
-                 dp[i, j] = Math.Min(dp[i - 1, j], dp[i, j - 1]) + grid[i][j];
-                //dp[i, j] = Math.Min(dp[i - 1, j], dp[i, j - 1]) + grid[i - 1][j - 1];
-                 Console.WriteLine(i + " " + j + ": " + dp[i, j]);
+                if(i == 0 && j > 0)
+                    grid[i][j] += grid[i][j - 1];
+                else if(j == 0 && i > 0)
+                    grid[i][j] += grid[i - 1][j];
+                else if(i > 0 && j > 0)
+                    grid[i][j] += Math.Min(grid[i - 1][j], grid[i][j - 1]);
             }
         }
         
-        return dp[m - 1, n - 1];
+        return grid[m - 1][n - 1];
     }
     
 //     private static int[] rowDirections = new int[]{ 0, -1, 0, 1};
