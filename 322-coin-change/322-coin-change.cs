efffa58022b7dba835c4d7d4101bfcc21220ab4b
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-27 14:01:45 +0300

    Time: 108 ms (83.39%), Space: 39.6 MB (69.45%) - LeetHub

--- 322-coin-change/322-coin-change.cs
+++ 322-coin-change/322-coin-change.cs
@@ -1,36 +1,52 @@
-public class Solution {
+public class Solution 
+{
     public int CoinChange(int[] coins, int amount) 
     {
-        var visited = new HashSet<int>();
-        var queue = new Queue<int>();
-        queue.Enqueue(0);
-        var distance = 0;
+        var dp = new int[amount + 1];
         
-        while(queue.Count() > 0)
+        for(int i = 1; i <= amount; i++)
         {
-            var size = queue.Count();
-            while(size-- > 0)
+            dp[i] = int.MaxValue;
+            for(int j = 0; j < coins.Length; j++)
             {
-                var current = queue.Dequeue();
-                
-                if(current == amount)
-                    return distance;
-                
-                foreach(var coin in coins)
-                {
-                    var newAmount = current + coin;
-                    
-                    if(newAmount <= amount && !visited.Contains(newAmount))
-                    {
-                        visited.Add(newAmount);
-                        queue.Enqueue(newAmount);
-                    }
-                }
+                if(coins[j] <= i &&  dp[i - coins[j]] != int.MaxValue)
+                    dp[i] = Math.Min(dp[i], dp[i - coins[j]] + 1);
             }
-            
-            distance++;
         }
         
-        return -1;
+        return dp[amount] == int.MaxValue ? -1 : dp[amount];
     }
+    
+    
+    //         var visited = new HashSet<int>();
+//         var queue = new Queue<int>();
+//         queue.Enqueue(0);
+//         var distance = 0;
+        
+//         while(queue.Count() > 0)
+//         {
+//             var size = queue.Count();
+//             while(size-- > 0)
+//             {
+//                 var current = queue.Dequeue();
+                
+//                 if(current == amount)
+//                     return distance;
+                
+//                 foreach(var coin in coins)
+//                 {
+//                     var newAmount = current + coin;
+                    
+//                     if(newAmount <= amount && !visited.Contains(newAmount))
+//                     {
+//                         visited.Add(newAmount);
+//                         queue.Enqueue(newAmount);
+//                     }
+//                 }
+//             }
+            
+//             distance++;
+//         }
+        
+//         return -1;
 }
