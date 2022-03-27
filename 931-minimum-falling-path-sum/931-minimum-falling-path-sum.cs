9b73ccdb7cbfdbaeb29d4b6ade806f9780176a61
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-27 14:46:00 +0300

    Time: 172 ms (17.65%), Space: 39.5 MB (96.79%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int MinFallingPathSum(int[][] matrix) 
+    {
+       for(int i = matrix.Length - 2; i >= 0; i--)
+       {
+           for(int j = 0; j < matrix[i].Length; j++)
+           {
+               if(j == 0)
+                   matrix[i][j] += Math.Min(matrix[i + 1][j], matrix[i + 1][j + 1]);
+               
+               if(j == matrix[i].Length - 1)
+                   matrix[i][j] += Math.Min(matrix[i + 1][j], matrix[i + 1][j - 1]);
+               
+               if(j > 0 && j < matrix[i].Length - 1)
+                   matrix[i][j] += Math.Min(Math.Min(matrix[i + 1][j], matrix[i + 1][j - 1]), matrix[i + 1][j + 1]);
+           }
+       }
+        
+        return matrix[0].Min();
+    }
+}
