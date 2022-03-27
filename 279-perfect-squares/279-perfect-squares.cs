b6a67f03c8352912c86327a90050e841581234de
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-27 13:54:08 +0300

    Time: 493 ms (21.36%), Space: 28 MB (53.07%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cs
+++ 279-perfect-squares/279-perfect-squares.cs
@@ -1,47 +1,65 @@
-public class Solution {
+public class Solution 
+{
     public int NumSquares(int n) 
     {
-        var square = new List<int>();
-        for(int i = 1; i * i <= n; i++)
-            square.Add(i * i);
+        var dp = new int[n + 1];
         
-        var queue = new Queue<int>();
-        queue.Enqueue(0);
-        
-        var distance = 1;
-        var visited = new HashSet<int>();
-        
-        if(visited.Contains(n))
-            return 1;
-        
-        while(queue.Count() > 0)
+        for(int i = 1; i <= n; i++)
         {
-            var size = queue.Count();
-            while(size-- > 0)
-            {
-                var current = queue.Dequeue();
+            dp[i] = int.MaxValue;
+            for(int j = 1; j * j <= i; j++)
+                dp[i] = Math.Min(dp[i], dp[i - j*j] + 1);
             
-                 if(current == n)
-                    return distance;
-                
-                foreach(var num in square)
-                {
-                    var newNum = current + num;
-                    
-                     if(newNum == n)
-                        return distance;
-                    
-                    if(newNum < n && !visited.Contains(newNum))
-                    {
-                        queue.Enqueue(newNum);
-                        visited.Add(newNum);
-                    }
-                }
-            }
-            
-            distance++;
+            Console.WriteLine();
         }
         
-        return distance;
+        return dp[n];
+        
+        
+        
+        
+        
+//         var square = new List<int>();
+//         for(int i = 1; i * i <= n; i++)
+//             square.Add(i * i);
+        
+//         var queue = new Queue<int>();
+//         queue.Enqueue(0);
+        
+//         var distance = 1;
+//         var visited = new HashSet<int>();
+        
+//         if(visited.Contains(n))
+//             return 1;
+        
+//         while(queue.Count() > 0)
+//         {
+//             var size = queue.Count();
+//             while(size-- > 0)
+//             {
+//                 var current = queue.Dequeue();
+            
+//                  if(current == n)
+//                     return distance;
+                
+//                 foreach(var num in square)
+//                 {
+//                     var newNum = current + num;
+                    
+//                      if(newNum == n)
+//                         return distance;
+                    
+//                     if(newNum < n && !visited.Contains(newNum))
+//                     {
+//                         queue.Enqueue(newNum);
+//                         visited.Add(newNum);
+//                     }
+//                 }
+//             }
+            
+//             distance++;
+//         }
+        
+//         return distance;
     }
 }
