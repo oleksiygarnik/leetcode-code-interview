b3b90fac2e8be13a69a4583214fe6d88a2098ecc
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-03-31 12:37:55 +0300

    Time: 112 ms (57.16%), Space: 37.8 MB (52.56%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) 
+    {
+        int left = 0, right = nums.Length - 1;
+        while(left <= right)
+        {
+            var middle = left + (right - left)/2;
+            if(nums[middle] == target)
+                return middle;
+            
+            if(nums[middle] > target)
+                right = middle - 1;
+            else
+                left = middle + 1;
+        }
+        
+        return left;
+    }
+}
