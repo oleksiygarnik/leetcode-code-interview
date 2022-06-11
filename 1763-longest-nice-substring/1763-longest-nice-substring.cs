63288abc3cf17c6b7125f621d223973054362f91
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-06-11 14:03:20 +0300

    Time: 268 ms (6.56%), Space: 39.8 MB (11.48%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.cs
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cs
@@ -1,11 +1,19 @@
 public class Solution {
     public string LongestNiceSubstring(string s) 
     {
+        var unique = GetUniqueChars(s);
+        
         var max = string.Empty;
         for(int i = 0; i < s.Length; i++)
         {
+            if(unique.Contains(Char.ToLower(s[i])))
+                continue;
+            
             for(int j = i; j < s.Length; j++)
             {
+                if(unique.Contains(Char.ToLower(s[j])))
+                    break;
+                
                 var substring = s.Substring(i, j - i + 1);
                 
                 if(IsNice(substring))
@@ -20,6 +28,24 @@ public class Solution {
         return max;
     }
 
+    private List<char> GetUniqueChars(string s)
+    {
+        var dict = new Dictionary<char, int>();
+        foreach(var ch in s)
+        {
+            var key = Char.ToLower(ch);
+            
+            if(dict.ContainsKey(key))
+                dict[key]++;
+            else
+                dict.Add(key, 1);
+        }
+        
+        return dict.Where(x => x.Value == 1)
+            .Select(x => x.Key)
+            .ToList();
+    }
+    
     private bool IsNice(string s)
     {
         var missing = new HashSet<char>();
