94e1c40f9a822b646a6ec6de7b4babdc2c306275
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-10-23 13:27:31 +0300

    Time: 480 ms (65.91%), Space: 50.4 MB (52.27%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cs
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int MinPairSum(int[] nums) 
     {
-        Array.Sort(nums);
+        QuickSort(nums, 0, nums.Length - 1);
         var left = 0;
         var right = nums.Length - 1;
         
@@ -10,5 +10,38 @@ public class Solution {
             maxValue = Math.Max(maxValue, nums[left++] + nums[right--]);
         
         return maxValue;
+    }
+    
+    private int[] QuickSort(int[] array, int leftIndex, int rightIndex)
+    {
+        var i = leftIndex;
+        var j = rightIndex;
+        var pivot = array[leftIndex];
+        
+        while(i <= j)
+        {
+            while(array[i] < pivot)
+                i++;
+            
+            while(array[j] > pivot)
+                j--;
+            
+            if(i <= j)
+            {
+                var tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                
+                i++;
+                j--;
+            }
+        }
+        
+        if(i < rightIndex)
+            QuickSort(array, i, rightIndex);
+        if(j > leftIndex)
+            QuickSort(array, leftIndex, j);
+        
+        return array;
     }
 }
