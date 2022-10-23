6a288eb91fff01d8d3037f11d5ce1148d6915997
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-10-23 17:18:09 +0300

    Time: 85 ms (97.85%), Space: 40 MB (5.38%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int FindFinalValue(int[] nums, int original) 
     {
-        Array.Sort(nums);
+        QuickSort(nums, 0, nums.Length - 1);
         for(int i = 0; i < nums.Length; i++)
         {
             if(BinarySearch(nums, i, nums.Length - 1, original) != -1)
@@ -26,5 +26,37 @@ public class Solution {
         }
         
         return - 1;
+    }
+    
+    private int[] QuickSort(int[] nums, int leftIndex, int rightIndex)
+    {
+        var i = leftIndex;
+        var j = rightIndex;
+        var pivot = nums[leftIndex];
+        
+        while(i <= j)
+        {
+            while(nums[i] < pivot)
+                i++;
+            while(nums[j] > pivot)
+                j--;
+            
+            if(i <= j)
+            {
+                var tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                
+                i++;
+                j--;
+            }
+        }
+        
+        if(i < rightIndex)
+            QuickSort(nums, i, rightIndex);
+        if(j > leftIndex)
+            QuickSort(nums, leftIndex, j);
+        
+        return nums;
     }
 }
