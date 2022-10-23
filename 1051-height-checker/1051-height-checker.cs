a73908ef98eec27e31a5cf761d45935ac469bfc3
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-10-23 12:56:50 +0300

    Time: 110 ms (78.99%), Space: 36.6 MB (91.12%) - LeetHub

--- 1051-height-checker/1051-height-checker.cs
+++ 1051-height-checker/1051-height-checker.cs
@@ -3,7 +3,7 @@ public class Solution {
     {
         var copy = new int[heights.Length];
         Array.Copy(heights, copy, heights.Length);
-        Array.Sort(heights);
+        QuickSort(heights, 0, heights.Length - 1);
         
         var result = 0;
         for(int i = 0; i < heights.Length; i++)
@@ -13,5 +13,37 @@ public class Solution {
         }
         
         return result;
+    }
+    
+    private int[] QuickSort(int[] array, int leftIndex, int rightIndex)
+    {
+        var i = leftIndex;
+        var j = rightIndex;
+        var pivot = array[leftIndex];
+        
+        while(i <= j)
+        {
+            while (array[i] < pivot)
+                i++;
+        
+            while (array[j] > pivot)
+                j--;
+            
+            if (i <= j)
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        
+        if (leftIndex < j)
+            QuickSort(array, leftIndex, j);
+        if (i < rightIndex)
+            QuickSort(array, i, rightIndex);
+        
+        return array;
     }
 }
