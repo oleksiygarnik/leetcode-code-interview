e30c5b028142acc0e8756ad16b4bfa2ae38bd6ec
Oleksii Harnyk <44923163+oleksiygarnik@users.noreply.github.com>
2022-10-23 17:13:17 +0300

    Time: 164 ms (31.18%), Space: 37.9 MB (91.40%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cs
@@ -1,10 +1,30 @@
 public class Solution {
     public int FindFinalValue(int[] nums, int original) 
     {
-        var set = new HashSet<int>(nums);
-        while(set.Contains(original))
-            original *= 2;
+        Array.Sort(nums);
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(BinarySearch(nums, i, nums.Length - 1, original) != -1)
+                original *= 2;
+        }
         
         return original;
+    }
+    
+    private int BinarySearch(int[] arr, int left, int right, int original)
+    {
+        while(left <= right)
+        {
+            var mid = left + (right - left)/2;
+            if(arr[mid] == original)
+                return mid;
+            
+            if(arr[mid] < original)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        
+        return - 1;
     }
 }
